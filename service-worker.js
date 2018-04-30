931deb8ee51eb5b8acdf26286958af20bcf1a944
Bryan McBride <mcbride.bryan@gmail.com>
2018-04-30 17:15:21 -0400

    Remove upup dependency

---
+++ service-worker.js
@@ -0,0 +1,95 @@
+/*jshint esversion: 6 */
+/*
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the 'License');
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an 'AS IS' BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+// Names of the two caches used in this version of the service worker.
+// Change to v2, etc. when you update any of the local resources, which will
+// in turn trigger the install event again.
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+  'index.html',
+  './', // Alias for index.html
+  'manifest.json',
+  'assets/img/geolocation_marker.png',
+  'assets/img/geolocation_marker_heading.png',
+  'assets/img/apple-touch-icon.png',
+  'assets/img/favicon-32x32.png',
+  'assets/img/favicon-16x16.png',
+  'assets/img/android-chrome-192x192.png',
+  'assets/vendor/framework7-2.2.5/css/framework7.min.css',
+  'assets/vendor/openlayers-4.6.5/ol.css',
+  'assets/css/app.css',
+  'assets/fonts/MaterialIcons-Regular.woff2',
+  'assets/vendor/framework7-2.2.5/js/framework7.min.js',
+  'assets/vendor/localForage-1.7.1/localforage.min.js',
+  'assets/vendor/openlayers-4.6.5/ol.js',
+  'assets/vendor/proj4js-2.4.3/proj4.js',
+  'assets/js/app.js'
+];
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        /* Disable runtime caching
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+        */
+
+        return fetch(event.request).then(response => {
+          return response;
+        });
+      })
+    );
+  }
+});
