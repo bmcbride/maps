b2c787e860f407f07bbcea4c4235757981634037
Bryan McBride <mcbride.bryan@gmail.com>
2018-08-27 17:00:33 -0400

    Update service worker

--- service-worker.js
+++ service-worker.js
@@ -2,20 +2,16 @@
 /*
  Copyright 2016 Google Inc. All Rights Reserved.
  Licensed under the Apache License, Version 2.0 (the 'License');
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-     http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an 'AS IS' BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+ You may not use this file except in compliance with the License.
+ You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and limitations under the License.
 */
 
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v3';
+const PRECACHE = 'precache-v4';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
@@ -27,13 +23,14 @@ const PRECACHE_URLS = [
   'assets/img/favicon-32x32.png',
   'assets/img/favicon-16x16.png',
   'assets/img/android-chrome-192x192.png',
-  'assets/vendor/framework7-3.0.1/css/framework7.min.css',
-  'assets/vendor/openlayers-5.0.3/ol.css',
+  'assets/img/crosshair.svg',
+  'assets/vendor/framework7-3.1.1/css/framework7.min.css',
+  'assets/vendor/openlayers-5.1.3/ol.css',
   'assets/css/app.css',
   'assets/fonts/MaterialIcons-Regular.woff2',
-  'assets/vendor/framework7-3.0.1/js/framework7.min.js',
+  'assets/vendor/framework7-3.1.1/js/framework7.min.js',
   'assets/vendor/localForage-1.7.1/localforage.min.js',
-  'assets/vendor/openlayers-5.0.3/ol.js',
+  'assets/vendor/openlayers-5.1.3/ol.js',
   'assets/vendor/proj4js-2.4.3/proj4.js',
   'assets/js/app.js'
 ];
