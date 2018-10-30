1ccdd1d1c085da05c6193039adbb04858444571b
Bryan McBride <mcbride.bryan@gmail.com>
2018-10-30 12:21:57 -0400

    Upgrade to localForage 1.7.3

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-10.27.18.1';
+const PRECACHE = 'precache-10.30.18.1';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
@@ -29,7 +29,7 @@ const PRECACHE_URLS = [
   'assets/css/app.css',
   'assets/fonts/MaterialIcons-Regular.woff2',
   'assets/vendor/framework7-3.4.2/js/framework7.min.js',
-  'assets/vendor/localForage-1.7.1/localforage.min.js',
+  'assets/vendor/localForage-1.7.3/localforage.min.js',
   'assets/vendor/openlayers-5.2.0/ol.js',
   'assets/vendor/proj4js-2.5.0/proj4.js',
   'assets/js/app.js'
